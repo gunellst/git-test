ba4112c4d7496b95e336554dc75712088f4acfca
Gunel <116121857+gunellst@users.noreply.github.com>
2024-09-13 14:44:17 +0400

    Add files via upload

---
+++ practise/script.js
@@ -0,0 +1,22 @@
+// document.getElementById('btn').addEventListener('click', function() {
+//     const a = document.getElementById('inp1').value;
+//     const b = document.getElementById('inp2').value;
+//     const sum = parseInt(a) + parseInt(b);
+//     document.getElementById('text').innerText = sum;
+//     document.getElementById('text').classList.add('title')
+//     document.getElementById('btn').setAttribute('title', 'gnl')
+
+//     console.log(sum);
+    
+// })
+
+
+
+
+
+// let btn = document.getElementById('clickadd');
+// btn.addEventListener('click', function() {
+//    let inpvalue =  document.getElementById('inp').value;
+//    let pvalue = document.getElementById('text').innerText
+//    document.getElementById('text').innerText = pvalue + ' ' + inpvalue;
+// })
